e5e44d849158196a90e078cbdf10f69334fc7e43
Agnaldo Brito <agnaldo.jnr1995@gmail.com>
2022-04-12 17:27:31 -0300

    limpando o código e adicionando a soma dos itens

--- js/checkItem.js
+++ js/checkItem.js
@@ -3,24 +3,43 @@ export function checkItem() {
   checkBox.type = 'checkbox';
   checkBox.classList.add('checkBtn');
   checkBox.addEventListener('click', itemCheck);
+
   return checkBox;
 }
 
 function itemCheck(e) {
   const itemChecked = e.target.parentElement;
   itemChecked.classList.toggle('checked');
-  if (itemChecked.classList.contains('checked')) {
-    insertPrice();
-  }
+
+  insertPrice(itemChecked);
 }
 
-function insertPrice() {
-  const popUpDiv = document.querySelector('.popUp');
-  popUpDiv.classList.remove('hidden');
-  const popUpBtn = document.querySelector('.popUp__btn');
-  popUpBtn.addEventListener('click', () => {
-    const popUpInput = document.querySelector('.popUp__input');
-    let total = parseFloat(document.querySelector('.total__price').value);
-    console.log(total);
-  });
+function insertPrice(itemChecked) {
+  itemChecked.classList.contains('checked')
+    ? popUpDiv.classList.remove('hidden')
+    : popUpDiv.classList.add('hidden');
 }
+
+const popUpDiv = document.querySelector('.popUp');
+const popUpBtn = document.querySelector('.popUp__btn');
+const showTotal = document.querySelector('.total__price');
+
+const array = [];
+
+let total = 0;
+popUpBtn.addEventListener('click', () => {
+  const popUpInput = document.querySelector('.popUp__input');
+  array.push(parseFloat(popUpInput.value));
+  console.log(array);
+  total = array.reduce((prev, curr) => {
+    return prev + curr;
+  }, 0);
+  const convertedValue = total.toLocaleString('pt-br', {
+    style: 'currency',
+    currency: 'BRL'
+  });
+
+  showTotal.innerText = `${convertedValue}`;
+  popUpDiv.classList.add('hidden');
+  popUpInput.value = null;
+});
