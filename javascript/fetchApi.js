b87e11e57587b10c89edbb0145e65cdc497713b1
Agnaldo Brito <agnaldo.jnr1995@gmail.com>
2022-04-14 20:34:31 -0300

    adicionando um timer pro gif

--- javascript/fetchApi.js
+++ javascript/fetchApi.js
@@ -5,19 +5,19 @@ export async function fetchApi(name) {
       `https://api.giphy.com/v1/gifs/search?api_key=dpPu1kIHwa3fxoQiH9lzTfmUkMgEjtuS&limit=1&q=${nameTrim}`
     );
     const responseJson = await response.json();
-    console.log(responseJson.data);
-    console.log('NAME', responseJson.data[0].title);
     console.log('META', responseJson.meta);
     if (responseJson.meta.msg === 'OK') {
       const fig = document.createElement('figure');
       const gif = document.createElement('img');
-      const fc = document.createElement('figcaption');
+
       gif.src = responseJson.data[0].images.downsized.url;
-      gif.alt = responseJson.data[0].title;
-      fc.textContent = responseJson.data[0].title;
+
       document.body.appendChild(fig);
       fig.appendChild(gif);
-      fig.appendChild(fc);
+
+      setTimeout(() => {
+        fig.remove();
+      }, 4250);
     } else {
       throw new Error('algo deu errado');
     }
