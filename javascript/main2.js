7c7d4a935e14033eb64e5137fa5cdf7f13669e3c
Agnaldo Brito <agnaldo.jnr1995@gmail.com>
2022-04-14 08:31:29 -0300

    Adicionando GIF

--- javascript/main2.js
+++ javascript/main2.js
@@ -106,6 +106,7 @@ function createItem(inputValue) {
     const dataArrayToString = JSON.stringify(dataArray);
     localStorage.setItem('storedData', dataArrayToString);
     createStatement(itemData.itemName, itemData.itemPrice);
+    fetchApi(itemData.itemName);
     calculateTotal();
     popUpContainer.remove();
   });
@@ -143,4 +144,23 @@ function createStatement(name, price) {
     currency: 'BRL'
   });
   list2.append(elementPrice);
+}
+
+async function fetchApi(name) {
+  const nameTrim = name.trim();
+  const response = await fetch(
+    `https://api.giphy.com/v1/gifs/search?api_key=dpPu1kIHwa3fxoQiH9lzTfmUkMgEjtuS&limit=1&q=${nameTrim}`
+  );
+  const responseJson = await response.json();
+  console.log(responseJson.data);
+  console.log('META', responseJson.meta);
+  const fig = document.createElement('figure');
+  const gif = document.createElement('img');
+  const fc = document.createElement('figcaption');
+  gif.src = responseJson.data[0].images.downsized.url;
+  gif.alt = responseJson.data[0].title;
+  fc.textContent = responseJson.data[0].title;
+  document.body.appendChild(fig);
+  fig.appendChild(gif);
+  fig.appendChild(fc);
 }
