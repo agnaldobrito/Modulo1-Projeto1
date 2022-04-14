19a087cbc6e705a520131d0f811ff98ea67841e7
Agnaldo Brito <agnaldo.jnr1995@gmail.com>
2022-04-14 20:35:58 -0300

    atualizando o main

--- javascript/main.js
+++ javascript/main.js
@@ -13,24 +13,28 @@ if (storedData != null) {
   dataArray.forEach(object => {
     const objectNameValue = object.itemName;
     const objectPriceValue = object.itemPrice;
+    //cria a lista do extrato depois de recarregar a página
     createStatement(objectNameValue, objectPriceValue);
   });
 }
 const inputItemName = document.querySelector('.header__content--input');
 const addBtn = document.querySelector('.header__content--btn');
 
+//////////botao que irá ativar o createItem()
 addBtn.addEventListener('click', () => {
   const value = inputItemName.value;
   if (value.length > 0) {
     //executa função se o input não estiver vazio
     createItem(value, dataArray);
     inputItemName.setAttribute('placeholder', 'Digite o nome do item aqui');
+    inputItemName.classList.remove('redLine');
   } else {
     //consertar
     inputItemName.setAttribute(
       'placeholder',
       'Por favor, digite o nome do item'
     );
+    inputItemName.classList.add('redLine');
   }
 
   inputItemName.value = '';
